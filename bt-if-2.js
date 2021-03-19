1a412e5ebe86db0980222d36e30fe86128c959b1
cuong0209992 <sandtimezone92@gmail.com>
2021-03-19 09:12:01 +0700

    Bài tập điều kiện 32

---
+++ bt-if-2.js
@@ -0,0 +1,36 @@
+/*//bài 1
+let doC = prompt('nhập nhiệt độ ');
+let doF;
+doF = doC * (9 / 5) +32;
+alert('độ F tương ứng là '+doF);
+*/
+
+// bài 2,3,4,5,6,7,8 tương tự
+
+/*//bài 9 kiểm tra có phải cạnh tam giác
+let a = +prompt('cạnh a')
+let b = +prompt('cạnh b')
+let c = +prompt('cạnh c')
+
+if(a<0 && b<0 && c<0){
+    alert('không là cạnh tam giác')
+}
+else if (a+b<=c){
+    alert('không là cạnh tam giác')
+}
+
+else if (b+c<=a){
+    alert('không là cạnh tam giác')
+}
+
+else if (a+c<=b){
+    alert('không là cạnh tam giác')
+}
+
+else {
+    alert('a,b,c là cạnh của 1 tam giác')
+}
+*/
+
+// bài 10, 11, 12 tương tự
+
